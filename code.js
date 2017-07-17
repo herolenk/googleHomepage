76d356c8a72120b2742d7f6396771d6db8194023
Mustafa Katipoğlu <herolenk@hotmail.com>
2017-07-17 16:27:46 +0300

    Delete unnecessary js

--- code.js
+++ code.js
@@ -2,13 +2,3 @@ document.getElementById("grid-btn").onclick = function(){
     document.getElementById("gogapp-container").style.display = "block"
 }
 
-//document.getElementById("grid-btn").onclick = show("gogapp-container");
-//
-//function show(Id){
-//    var openMenu = document.getElementById(Id);
-//    if(openMenu){
-//       openMenu.removeAttribute("id");
-//    }else{
-//        openMenu.setAttribute("id");
-//    }
-//}
